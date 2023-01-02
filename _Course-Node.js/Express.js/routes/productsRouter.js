180b50070f4310ed53af558d6b9e6425cd22d998
D4rkPK <brandonpineda4@gmail.com>
2023-01-02 15:34:55 -0600

    :art: [ADD] Joi

--- _Course-Node.js/Express.js/routes/productsRouter.js
+++ _Course-Node.js/Express.js/routes/productsRouter.js
@@ -1,6 +1,12 @@
 const express = require("express");
 
 const ProductService = require("../services/productService");
+const validatorHandler = require("../middlewares/validatorHandler");
+const {
+  createProductSchema,
+  updateProductSchema,
+  getProductSchema,
+} = require("../schemas/productSchema");
 
 const router = express.Router();
 const service = new ProductService();
@@ -17,7 +23,9 @@ router.get("/filter", (req, res) => {
   res.send("Filter");
 });
 
-router.get("/:id", async (req, res, next) => {
+router.get("/:id", 
+validatorHandler(getProductSchema, 'params'),
+async (req, res, next) => {
   try {
     const { id } = req.params;
     const product = await service.findOne(id);
@@ -30,7 +38,9 @@ router.get("/:id", async (req, res, next) => {
   }
 });
 
-router.post("/", async (req, res) => {
+router.post("/", 
+validatorHandler(createProductSchema, 'body'),
+async (req, res) => {
   const body = req.body;
   const newProduct = await service.create(body);
   res.status(201).json({
@@ -39,7 +49,10 @@ router.post("/", async (req, res) => {
   });
 });
 
-router.patch("/:id", async (req, res, next) => {
+router.patch("/:id", 
+validatorHandler(getProductSchema, 'params'),
+validatorHandler(updateProductSchema, 'body'),
+async (req, res, next) => {
   try {
     const { id } = req.params;
     const body = req.body;
@@ -53,7 +66,9 @@ router.patch("/:id", async (req, res, next) => {
   }
 });
 
-router.delete("/:id", async (req, res, next) => {
+router.delete("/:id", 
+validatorHandler(getProductSchema, 'params'),
+async (req, res, next) => {
   try {
     const { id } = req.params;
     const product = await service.delete(id);
--- _Course-Node.js/Express.js/schemas/productSchema.js
+++ _Course-Node.js/Express.js/schemas/productSchema.js
@@ -1,17 +1,20 @@
 const Joi = require("joi");
 
 const id = Joi.string().uuid();
-const name = Joistring().alphanum().min(3).max(30);
-const price = Joi.number().float().min(1);
+const name = Joi.string().min(3).max(30);
+const price = Joi.number().min(1);
+const image = Joi.string().uri();
 
 const createProductSchema = Joi.object({
   name: name.required(),
   price: price.required(),
+  image: image.required(),
 });
 
 const updateProductSchema = Joi.object({
   name: name,
   price: price,
+  image: image,
 });
 
 const getProductSchema = Joi.object({
@@ -19,7 +22,7 @@ const getProductSchema = Joi.object({
 });
 
 module.exports = {
-    createProductSchema,
-    updateProductSchema,
-    getProductSchema,
+  createProductSchema,
+  updateProductSchema,
+  getProductSchema,
 };
